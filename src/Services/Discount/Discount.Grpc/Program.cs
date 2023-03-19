95f9cc4197cf62040172f3231533d48edb1350da
nuttaporn kunsin <to_andata@hotmail.com>
2023-03-19 18:08:57 +0700

    successful Discount Grpc Service

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,17 +1,74 @@
+using Discount.Grpc.Repositories;
 using Discount.Grpc.Services;
+using Npgsql;
+using Polly;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
+builder.Services.AddScoped<IDiscountRepository,DiscountRepository>();
+builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddGrpc();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
+
+using(var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    try
+    {
+        var retry = Policy.Handle<NpgsqlException>()
+            .WaitAndRetry(retryCount: 5,
+                sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), // 2,4,8,16,32 sc
+                onRetry: (exception, retryCount, context) =>
+                {
+                    logger.LogError($"Retry {retryCount} of {context.PolicyKey} at {context.OperationKey}, due to: {exception}.");
+                });
+
+        retry.Execute(() => ExecuteMigrations(builder.Configuration));
+        logger.LogInformation("Migrated postresql database.");
+
+    }
+    catch (Exception ex)
+    {
+
+        logger.LogError(ex, "An error occurred sedding the database");
+    }
+}
+
+static void ExecuteMigrations(ConfigurationManager configuration)
+{
+    using NpgsqlConnection connection = new(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+    connection.Open();
+
+    using var cmd = new NpgsqlCommand
+    {
+        Connection = connection
+    };
+
+    cmd.CommandText = "DROP TABLE IF EXISTS Coupon";
+    cmd.ExecuteNonQuery();
+
+    cmd.CommandText = @"CREATE TABLE Coupon(
+                        Id SERIAL PRIMARY KEY, 
+                        ProductName VARCHAR(24) NOT NULL,
+                        Description TEXT,
+                        Amount INT)";
+    cmd.ExecuteNonQuery();
+
+    cmd.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
+    cmd.ExecuteNonQuery();
+
+    cmd.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
+    cmd.ExecuteNonQuery();
+}
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ src/Services/Discount/Discount.Grpc/Repositories/DiscountRepository.cs
@@ -0,0 +1,75 @@
+﻿using Dapper;
+using Discount.Grpc.Entities;
+using Npgsql;
+
+namespace Discount.Grpc.Repositories;
+
+public class DiscountRepository : IDiscountRepository
+{
+    private readonly IConfiguration _configuration;
+    private readonly string _connectionString;
+
+    public DiscountRepository(IConfiguration configuration)
+    {
+        _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        _connectionString = configuration.GetConnectionString("Discount") ?? throw new ArgumentNullException(nameof(configuration));
+    }
+    public async Task<Coupon> GetDiscount(string productName)
+    {
+        using NpgsqlConnection connection = new(_connectionString);
+        var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>
+            ("SELECT * FROM Coupon WHERE ProductName = @ProductName",new {ProductName = productName});
+
+        if (coupon == null)
+        {
+            return new Coupon
+            {
+                ProductName = "No Discount",
+                Amount = 0,
+                Description = string.Empty,
+            };
+        }
+        return coupon;
+    }
+
+    public async Task<bool> CreateDiscount(Coupon coupon)
+    {
+        using NpgsqlConnection connection = new(_connectionString);
+        var affected = await connection.ExecuteAsync
+            ("INSERT INTO Coupon (ProductName,Description,Amount) VALUES (@ProductName,@Description,@Amount)",
+            new
+            {
+                ProductName = coupon.ProductName,
+                Description = coupon.Description,
+                Amount = coupon.Amount,
+            });
+
+        return affected > 0;
+    }
+
+    public async Task<bool> UpdateDiscount(Coupon coupon)
+    {
+        using NpgsqlConnection connection = new(_connectionString);
+        var affected = await connection.ExecuteAsync
+            ("UPDATE Coupon set ProductName = @ProductName,Description = @Description,Amount=@Amount WHERE Id=@Id",
+            new
+            {
+                Id = coupon.Id,
+                ProductName = coupon.ProductName,
+                Description = coupon.Description,
+                Amount = coupon.Amount,
+            });
+
+        return affected > 0;
+    }
+
+    public async Task<bool> DeleteDiscount(string productName)
+    {
+        using NpgsqlConnection connection = new(_connectionString);
+        var affected = await connection.ExecuteAsync
+            ("DELETE FROM Coupon WHERE ProductName = @ProductName", new { ProductName = productName });
+
+        return affected > 0;
+
+    }
+}
